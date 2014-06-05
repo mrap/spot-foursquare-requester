5d3cb9059973ca8840864a396473e2876892c430
Michael Rapadas <mike@mrap.me>
2014-06-04 23:59:21 -0700

    Updates production to use mongohq

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ var mongoose        = require('mongoose')
   , FoursquarePhoto = require('./models/foursquare_photo')
   , FoursquareVenue = require('./models/foursquare_venue.js');
 
-var uri  = 'mongodb://localhost/spot_foursquare_requester';
+var uri = (process.env.NODE_ENV == 'production') ? process.env.MONGOHQ_URL : 'mongodb://localhost/spot_foursquare_requester';
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(){
   console.log("Connected to MongoDB@%s", uri);
